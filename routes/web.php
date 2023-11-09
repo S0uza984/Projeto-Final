b9a7bd2ee227ea820a2a1ac58cca3732526b4061
GuiMedss <142314475+GuiMedss@users.noreply.github.com>
2023-11-09 16:21:55 -0300

    backdelete

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\AniversarianteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-
-Route::get('/login', [AuthLoginController::class, 'index'])->name('login.index');
-
-Route::get('/aniversariantes', [AniversarianteController::class, 'index'])->name('aniversariantes.index');
 
 Route::get('/', function () {
     return view('welcome');
